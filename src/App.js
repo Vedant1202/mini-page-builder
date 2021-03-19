3e00b5340edbec0656c54ca66898719f84cf4250
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2021-03-19 21:32:34 +0530

    Add redux store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+/** @format */
+
+import 'antd/dist/antd.css';
 import './App.css';
+import DragDropPage from './pages/dragdrop/dragdrop.pages';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <DragDropPage />
+        </div>
+    );
 }
 
 export default App;
